5303708e1f86e44d1b945d286ac25565b05a5fbc
Spencer Truett <srtruett1993@gmail.com>
2021-03-05 09:19:30 -0600

    basic app rendering on page

---
+++ src/components/MatchupTitle.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <dvi>
+      <h2>{{team1.name}}</h2>
+      <div>
+        <button>-</button>
+        <button>+</button>
+      </div>
+    </dvi>
+    <div>
+      <h2>{{team2.name}}</h2>
+      <button>-</button>
+      <button>+</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      team1: {
+        name: "Team 1",
+        score: 0
+      },
+      team2: {
+        name: "Team 2",
+        score: 0
+      }
+    };
+  }
+};
+</script>
+
+<style>
+</style>
